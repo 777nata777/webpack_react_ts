6260f23da1c6d186360bd3d84bd4a4631bab05ad
777nata777 <m-eme@yandex.ru>
2025-02-01 21:42:40 +0300

    работает с scss.module в import * as git status!

--- config/buildLoader.ts
+++ config/buildLoader.ts
@@ -5,8 +5,26 @@ import {BuildOptions} from "./types/types";
 
 export function buildLoader(options: BuildOptions): ModuleOptions ['rules'] {
     return [
+
+        {
+            test: /\.module\.s[ac]ss$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: "css-loader",
+                    options: {
+                        modules: {
+                            localIdentName: options.isDev ? "[path][name]__[local]" : "[hash:base64:8]",
+                        },
+                    },
+                },
+                "sass-loader",
+            ],
+        },
+
         {
             test: /\.s[ac]ss$/i,
+            exclude: /\.module\.s[ac]ss$/i,
             use: [
                 // Creates `style` nodes from JS strings
                 MiniCssExtractPlugin.loader,
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,19 @@
 import React, {useState} from 'react';
-import './App.scss';
+import * as styles from './App.module.scss';
+
 
 const App = () => {
     const [count, setCount] = useState<number>(0);
 
     const increment = () => setCount(prev => prev + 1)
 
+    console.log("CSS Modules:", styles);
+
     return (
+
         <div>
             <h1>{count}</h1>
-            <button onClick={increment}><span>
+            <button className={styles.button} onClick={increment}><span>
                 asdf
             </span></button>
         </div>
